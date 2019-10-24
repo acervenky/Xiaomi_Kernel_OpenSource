80f9c4d3eb7c7801ca3fd005433290dcf2fb2769
Suzuki K Poulose <suzuki.poulose@arm.com>
2019-10-24 14:48:11 +0200

    arm64: Add helpers for checking CPU MIDR against a range
    [ Upstream commit 1df310505d6d544802016f6bae49aab836ae8510 ]
    
    Add helpers for checking if the given CPU midr falls in a range
    of variants/revisions for a given model.
    
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Dave Martin <dave.martin@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/include/asm/cputype.h
+++ arch/arm64/include/asm/cputype.h
@@ -155,19 +155,25 @@ struct midr_range {
 };
 
 #define GENERIC_MIDR_RANGE(m, v_min, r_min, v_max, r_max)	\
+#define MIDR_RANGE(m, v_min, r_min, v_max, r_max)		\
 	{							\
 		.model = m,					\
 		.rv_min = MIDR_CPU_VAR_REV(v_min, r_min),	\
 		.rv_max = MIDR_CPU_VAR_REV(v_max, r_max),	\
 	}
 
+
 #define GENERIC_MIDR_ALL_VERSIONS(m) GENERIC_MIDR_RANGE(m, 0, 0, 0xf, 0xf)
+
+#define MIDR_ALL_VERSIONS(m) MIDR_RANGE(m, 0, 0, 0xf, 0xf)
+
 
 static inline bool is_midr_in_range(u32 midr, struct midr_range const *range)
 {
 	return MIDR_IS_CPU_MODEL_RANGE(midr, range->model,
 				 range->rv_min, range->rv_max);
 }
+
 
 static inline bool
 is_midr_in_range_list(u32 midr, struct midr_range const *ranges)
@@ -177,6 +183,7 @@ is_midr_in_range_list(u32 midr, struct midr_range const *ranges)
 			return true;
 	return false;
 }
+
 
 /*
  * The CPU ID never changes at run time, so we might as well tell the
